26a2ce48a40e1c1eb49506611558a552cadecff5
Ilya Chernyaev <iachernyaev@avito.ru>
2023-10-03 14:52:29 +0300

    sdf

--- internal/service/handlers.go
+++ internal/service/handlers.go
@@ -33,7 +33,7 @@ func (s *service) ValueHandler(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
-		newValue := value.(float64)
+		newValue := float64(value.(int64))
 		mdata.Value = &newValue
 		s.log.Debug("VAL: gauge ", mdata.ID, " value:", mdata.Value)
 	} else if mdata.MType == `counter` {
